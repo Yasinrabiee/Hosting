406870ac7f00d9cd6e06b42c4897b293cecfa154
YasinRabiee <hlww6303@gmail.com>
2025-02-02 20:08:46 -0800

    add works and comments components

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,39 @@
-let btn = $(`.header__icon i`);
+const worksContent = {
+	props: ["header", "paragraph"],
+	template: `
+		<div class="works__content__item">
+		    <h4 class="item-header">{{ header }}</h4>
+		    <p class="item-text">
+		        {{ paragraph }}
+		    </p>
+		</div>
+	`,
+}
 
-$(`.header__icon`).click(function() {
-	if(btn.hasClass(`fa fa-bars`))
-	{
-		$(`.nav__menu`).css(`left`,`0`);
-		btn.removeClass(`fa fa-bars`).addClass(`fa fa-times`);
+const comment = {
+	props: ["img", "name", "comment"],
+	template: `
+		<div class="comment">
+		    <div class="box-comments__body">
+		        <p class="box-comments__content">{{ comment }}</p>
+		    </div>
+		    <div class="box-comments__info">
+		        <img :src="img" alt="img info" class="box-comments__img">
+		        <p class="box-comments__name">{{ name }}</p>
+		    </div>
+		</div>
+	`,
+}
+
+new Vue({
+	el: ".box-comments",
+	components: {
+		comment
 	}
-	
-	else
-	{
-		$(`.nav__menu`).css(`left`,`-255px`);
-		btn.removeClass(`fa fa-times`).addClass(`fa fa-bars`);
+});
+new Vue({
+	el: ".works__content",
+	components: {
+		worksContent
 	}
-})
+});
